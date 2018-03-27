eb910dc0fc89b7e1a7a0536714d241512b700ac1
Karol <naerriel@gmail.com>
2018-03-27 14:11:16 +0200

    Change names.

--- src/event_page.js
+++ src/event_page.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({
-    'url': chrome.extension.getURL('src/View/menu.html')
+    'url': chrome.extension.getURL('src/view/menu.html')
   }, function(tab) {});
 });
