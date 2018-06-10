ac999830a8a64244aea02e9859c64f272e6ce60f
Karol Gorski <naerriel@gmail.com>
2018-06-10 12:49:05 +0200

    Add skill removal confirmation

--- src/Components/Skill/Skill.js
+++ src/Components/Skill/Skill.js
@@ -11,7 +11,8 @@ class Skill extends Component {
       skillInfo: props.skillInfo,
       edit: props.edit,
       progress: Math.floor(props.skillInfo.exp / props.skillInfo.expTillNextLevel * 100),
-      addExp: 1
+      addExp: 1,
+      startedRemoval: false
     };
     //Ustaw, że minimalny progress to jest jakieś 5%, żeby napis "0%" się zmieścił :P
   }
@@ -38,40 +39,76 @@ class Skill extends Component {
     this.props.addExp(this.state.addExp, this.state.skillInfo.name);
   }
 
-  startRemovingSkill = () => {
+  startSkillRemoval = () => {
+    this.setState({ startedRemoval: true });
+  }
+
+  confirmSkillRemoval = () => {
     this.props.removeSkill(this.state.skillInfo.name);
+  }
+
+  rejectSkillRemoval = () => {
+    this.setState({ startedRemoval: false });
   }
 
   editElements = () => {
     if(this.state.edit) {
-      return <button className="removeSkillBtn" onClick={this.startRemovingSkill}></button>
+      return <button className="removeSkillBtn" onClick={this.startSkillRemoval}></button>
     }
   }
 
   render() {
     return(
-      <div className="skill">
-        {this.editElements()}
-        <span className="level">{this.state.skillInfo.level}</span>
-        <span className="levelLabel">lvl</span>
-        <span className="skillName">{this.state.skillInfo.name}</span>
-        <div className="progressBarWrapper">
-          <span className="progressBarContainer">
-            <span className="progressBarFill">{this.state.progress}%</span>
-          </span>
-          <div className="progressBarBtns">
-            <input
-              className="addExpInput"
-              type="number"
-              value={this.state.addExp}
-              onChange={this.handleExpChange}
-              onKeyPress={this.handleKeyPress}
-            ></input>
-            <button className="addBtn" onClick={this.addExp}>+</button>
+      <div>
+        {this.state.startedRemoval ? (
+            <div>
+              <div className="screenDim"></div>
+              <div className="skillRemoveConfirmation">
+                <p className="skillRemoveMessage">
+                  Are you sure you want to remove
+                  <span className="skillName">{this.state.skillInfo.name}</span>
+                  skill?
+                </p>
+                  <div className="skillRemoveConfirmationButtons">
+                    <button
+                      className="skillRemoveConfirmationBtn"
+                      onClick={this.confirmSkillRemoval}
+                    >
+                    Yes
+                    </button>
+                    <button
+                      className="skillRemoveConfirmationBtn"
+                      onClick={this.rejectSkillRemoval}
+                    >
+                    No
+                    </button>
+                  </div>
+              </div>
+            </div>
+        ) : (<div></div>)}
+        <div className="skill">
+          {this.editElements()}
+          <span className="level">{this.state.skillInfo.level}</span>
+          <span className="levelLabel">lvl</span>
+          <span className="skillName">{this.state.skillInfo.name}</span>
+          <div className="progressBarWrapper">
+            <span className="progressBarContainer">
+              <span className="progressBarFill">{this.state.progress}%</span>
+            </span>
+            <div className="progressBarBtns">
+              <input
+                className="addExpInput"
+                type="number"
+                value={this.state.addExp}
+                onChange={this.handleExpChange}
+                onKeyPress={this.handleKeyPress}
+              ></input>
+              <button className="addBtn" onClick={this.addExp}>+</button>
+            </div>
           </div>
+          <span className="experience">{this.state.skillInfo.exp}/
+            {this.state.skillInfo.expTillNextLevel}</span>
         </div>
-        <span className="experience">{this.state.skillInfo.exp}/
-          {this.state.skillInfo.expTillNextLevel}</span>
       </div>
     );
   }
