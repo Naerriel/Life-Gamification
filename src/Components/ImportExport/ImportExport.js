33b4107e2055361b4c47091cb25f4fdaba5cbe13
Karol Gorski <naerriel@gmail.com>
2018-06-11 11:14:56 +0200

    Add static view for Import&Export

--- src/Components/ImportExport/ImportExport.js
+++ src/Components/ImportExport/ImportExport.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import "./ImportExport.css"
+
 class ImportExport extends Component {
 
   render() {
     return (
-      <div>
-        Hi, ImportExport here!
+      <div className="importExport">
+        <button className="importExportBtn">Import</button>
+        <button className="importExportBtn">Export</button>
+        <textarea
+          className="importExportJSONText"
+          placeholder="Place JSON here"
+        ></textarea>
       </div>
     );
   }
