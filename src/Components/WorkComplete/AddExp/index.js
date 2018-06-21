639452d37561f0741e9f7a4ae8fcfa94160b0d8d
Karol Gorski <naerriel@gmail.com>
2018-06-21 11:31:24 +0200

    Add static view to addingExp in WorkComplete

--- src/Components/WorkComplete/AddExp/index.js
+++ src/Components/WorkComplete/AddExp/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './index.css'
 
 import { connect} from 'react-redux'
 
@@ -8,10 +9,42 @@ class _AddExp extends Component {
     return 'expToAdd' in this.props.workComplete
   }
 
+  handleContinueBtn = () => {
+    console.log("Continuing")
+
+  }
+
   render() {
+    const { name, expAtATime } = this.props.workComplete
+
     if(this.shouldRender()){
       return (
-        <div> AddExp, yay!</div>
+        <div className="wholePage">
+          <div className="screenDim">
+          </div>
+          <div className="addExp">
+            <div className="finishedTask-main-message">
+              You add {expAtATime} exp to:
+              <div className="skill-name">
+                {name}
+              </div>
+            </div>
+            <div className="finishedTask-secondary-message">
+              Write what have you been working on:
+            </div>
+            <input
+              type="text"
+              className="finishedTask-log"
+              placeholder="E.g. Finished 3rd chapter"
+            />
+            <button
+              className="finishedTask-btn"
+              onClick={this.handleContinueBtn}
+            >
+              Continue
+            </button>
+          </div>
+        </div>
       )
     } else {
       return null
